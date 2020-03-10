8c29869e6b8b37b63b4df720adca2bff53a1ce6e
ExcelCodeM <1446896556@qq.com>
2020-03-11 00:18:46 +0800

    第一次提交

---
+++ scParent/cloud-provider-payment8001/src/main/java/com/test/Application.java
@@ -0,0 +1,17 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ：Breeze
+ * @date ：Created in 2020/3/11 0:14
+ * @description：
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}

1b7420342294ac6468787cf66e860e24d8c0b3fd
ExcelCodeM <1446896556@qq.com>
2020-06-30 23:55:22 +0800

    a

--- scParent/boot-security/src/main/java/com/test/config/SecurityOAuthConfig.java
+++ scParent/boot-security/src/main/java/com/test/config/SecurityOAuthConfig.java
@@ -3,12 +3,19 @@ package com.test.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.security.oauth2.provider.code.AuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.code.InMemoryAuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
@@ -27,14 +34,52 @@ public class SecurityOAuthConfig extends AuthorizationServerConfigurerAdapter {
     @Autowired
     private DataSource dataSource;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    /**
+     * 配置令牌存储方式，本次采用数据库存储
+     *
+     * @return
+     */
     @Bean
-    public TokenStore getTokenStore(){
+    public TokenStore getTokenStore() {
         return new JdbcTokenStore(dataSource);
     }
 
+    /**
+     * 配置客户端详情存储方式，本次采用数据库存储
+     *
+     * @return
+     */
     @Bean
-    public ClientDetailsService jdbcClientDetails(){
+    public ClientDetailsService jdbcClientDetails() {
         return new JdbcClientDetailsService(dataSource);
+    }
+
+    /**
+     * 接口定义了一些操作使得你可以对令牌进行一些必要的管理，令牌可以被用来 加载身份信息，里面包含了这个令牌的相关权限。
+     *
+     * @return
+     */
+    @Bean
+    public AuthorizationServerTokenServices tokenService() {
+        DefaultTokenServices service = new DefaultTokenServices();
+        service.setTokenStore(getTokenStore());  //设置令牌存储方式
+        service.setClientDetailsService(jdbcClientDetails());   //设置客户端存储方式
+        service.setSupportRefreshToken(true);   //是否支持刷新令牌
+        service.setAccessTokenValiditySeconds(60 * 60 * 2);     //设置令牌过期时间 2小时
+        service.setRefreshTokenValiditySeconds(60 * 60 * 24 * 3);   //设置刷新令牌的有效期 3天
+        return service;
+    }
+
+    /**
+     * 设置授权码服务
+     * @return
+     */
+    @Bean
+    public AuthorizationCodeServices authorizationCodeServices() { //设置授权码模式的授权码如何 存取，
+        return new JdbcAuthorizationCodeServices(dataSource);
     }
 
     @Override
@@ -55,6 +100,13 @@ public class SecurityOAuthConfig extends AuthorizationServerConfigurerAdapter {
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.tokenStore(getTokenStore());
+        endpoints.tokenStore(getTokenStore())
+                .tokenServices(tokenService())
+                .authenticationManager(authenticationManager)
+                .authorizationCodeServices(authorizationCodeServices())
+                .allowedTokenEndpointRequestMethods(HttpMethod.POST);
     }
+
+
+
 }

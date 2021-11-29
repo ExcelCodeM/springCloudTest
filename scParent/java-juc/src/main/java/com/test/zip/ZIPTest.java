4f797b22a5e519b34c394bd87f6880c748aaa17d
machunyu <jlo1390@dingtalk.com>
2021-11-29 16:14:13 +0800

    压缩文件

---
+++ scParent/java-juc/src/main/java/com/test/zip/ZIPTest.java
@@ -0,0 +1,21 @@
+package com.test.zip;
+
+import cn.hutool.core.io.FileUtil;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ZIPTest {
+
+    public static void main(String[] args) throws IOException {
+//        FileOutputStream fileOutputStream = new FileOutputStream("C:\\Users\\Administrator\\Desktop\\hehe.zip");
+//
+//        ZIPUtils.toZip("E:\\1232312312", fileOutputStream, true);
+
+        FileUtil.del(new File("E:\\234214243"));
+
+    }
+
+}
+
+

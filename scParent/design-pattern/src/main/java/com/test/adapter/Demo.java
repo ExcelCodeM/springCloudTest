565acb87515bdec247028c28e097847dc5f72ac2
machunyu <jlo1390@dingtalk.com>
2021-12-17 15:40:34 +0800

    适配器模式

---
+++ scParent/design-pattern/src/main/java/com/test/adapter/Demo.java
@@ -0,0 +1,31 @@
+package com.test.adapter;
+
+/**
+ * Somewhere in client code...
+ * <p>
+ * 实际上，就是实现了一个适配器类，实现了承上启下的作用
+ */
+public class Demo {
+    public static void main(String[] args) {
+        // Round fits round, no surprise.
+        RoundHole hole = new RoundHole(5);
+        RoundPeg rpeg = new RoundPeg(5);
+        if (hole.fits(rpeg)) {
+            System.out.println("Round peg r5 fits round hole r5.");
+        }
+
+        SquarePeg smallSqPeg = new SquarePeg(2);
+        SquarePeg largeSqPeg = new SquarePeg(20);
+        // hole.fits(smallSqPeg); // Won't compile.
+
+        // Adapter solves the problem.
+        SquarePegAdapter smallSqPegAdapter = new SquarePegAdapter(smallSqPeg);
+        SquarePegAdapter largeSqPegAdapter = new SquarePegAdapter(largeSqPeg);
+        if (hole.fits(smallSqPegAdapter)) {
+            System.out.println("Square peg w2 fits round hole r5.");
+        }
+        if (!hole.fits(largeSqPegAdapter)) {
+            System.out.println("Square peg w20 does not fit into round hole r5.");
+        }
+    }
+}

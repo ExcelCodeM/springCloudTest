bdf8d4b80b2b3caebc64234575205dd39ceea42b
machunyu <jlo1390@dingtalk.com>
2021-12-29 11:22:25 +0800

    命令模式

---
+++ scParent/design-pattern/src/main/java/com/test/command/Demo.java
@@ -0,0 +1,11 @@
+package com.test.command;
+
+/**
+ * 命令模式
+ */
+public class Demo {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.init();
+    }
+}
